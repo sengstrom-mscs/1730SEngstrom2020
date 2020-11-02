19fa2745bf83c07357ed9dc1a564c442cf7c3f2c
Stacey Engstrom <kg5466or@go.minnstate.edu>
2020-11-01 22:09:20 -0600

    Completed 2F

---
+++ sengstrom1730ex2F/1730ex2F.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sengstrom1730ex2F
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+
+            // #1: if            
+            result1TextBox.Text = Ex2fCalculations.Calc01(input1ATextBox.Text);
+
+            //#2: if {block}      
+            result2TextBox.Text = Ex2fCalculations.Calc02(input2ATextBox.Text);
+
+            //#3 if else
+            result3TextBox.Text = Ex2fCalculations.Calc03(input3ATextBox.Text);
+
+            //#4 if else if        
+            result4TextBox.Text = Ex2fCalculations.Calc04(input4ATextBox.Text);
+
+            //#5 Better range test         
+            result5TextBox.Text = Ex2fCalculations.Calc05(input5ATextBox.Text);
+
+            //#6 Nested if statements            
+            result6TextBox.Text = Ex2fCalculations.Calc06(input6ATextBox.Text, input6BTextBox.Text);
+
+            // #7 input validation
+            result7TextBox.Text = Ex2fCalculations.Calc07(input7ATextBox.Text);
+
+
+            // #8 Validate input, calculate quantity * price, shipping
+            result8TextBox.Text = Ex2fCalculations.Calc08(input8ATextBox.Text, input8BTextBox.Text);
+
+            // #9 Validate input, calculate difference * rate
+            result9TextBox.Text = Ex2fCalculations.Calc09(input9ATextBox.Text, input9BTextBox.Text);
+
+            // #10 Validate input, divide large num by small
+            //     Both numbers must be > 0
+            result10TextBox.Text = Ex2fCalculations.Calc10(input10ATextBox.Text, input10BTextBox.Text);
+
+        }
+
+    }  
+}
---
+++ sengstrom1730ex2FUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sengstrom1730ex2FUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sengstrom1730ex2FUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1528c4a7-7119-49ef-931c-aba1693f295e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
